e20624c0ab112c649ff8fe50d25752185fcf1367
Saikat Bhattacharya <saikatb@singnet.com.sg>
2021-06-05 15:11:38 +0800

    Added seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_05_013942) do
+ActiveRecord::Schema.define(version: 2021_06_05_044845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 2021_06_05_013942) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "budget"
+    t.integer "timeframe"
+    t.string "current_role"
+    t.string "current_industry"
+    t.string "future_role"
+    t.string "future_industry"
+    t.integer "available_hrs_per_week"
+    t.string "status"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
