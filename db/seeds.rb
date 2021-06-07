b548f7c06406bfd2f899e076f36a1de82f4e1a46
Saikat Bhattacharya <saikatb@singnet.com.sg>
2021-06-07 09:39:57 +0800

    Interim Results Page with Pre-Signup and PostSign-Up shows

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,58 +9,102 @@ require 'faker'
 
 User.destroy_all
 
-User.create!(
-  email: "user1@xyz.com",
-  password: "secret",
-    first_name: Faker::Name.female_first_name,
-    last_name: Faker::Name.last_name,
-    budget: [500, 1000, 1500, 2000, 2500, 3000].sample,
-    timeframe: [6, 12, 18, 24].sample,
-    current_role: "Health-care Worker",
-    current_industry: "Health Care",
-    future_role: "Product Manager",
-    future_industry: "Software Engineering",
-    available_hrs_per_week: [12, 24, 36, 40, 60].sample,
-    status: '');
+male_images = [ 
+  "https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 
+  "https://images.pexels.com/photos/2474661/pexels-photo-2474661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 
+  "https://images.pexels.com/photos/2087748/pexels-photo-2087748.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 
+  "https://images.pexels.com/photos/4194390/pexels-photo-4194390.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 
+  "https://images.pexels.com/photos/1437267/pexels-photo-1437267.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 
+  "https://images.pexels.com/photos/884600/pexels-photo-884600.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 
+  "https://images.pexels.com/photos/2098085/pexels-photo-2098085.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" 
+]
 
-User.create!(
-  email: "user2@xyz.com",
-  password: "secret",
-    first_name: Faker::Name.male_first_name,
-    last_name: Faker::Name.last_name,
-    budget: [500, 1000, 1500, 2000, 2500, 3000].sample,
-    timeframe: [6, 12, 18, 24].sample,
-    current_role: "Wealth Planner",
-    current_industry: "Banking",
-    future_role: "Web Developer",
-    future_industry: "Software Engineering",
-    available_hrs_per_week: [12, 24, 36, 40, 60].sample,
-    status: '');
+female_images = [ 
+  "https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 
+  "https://images.pexels.com/photos/2474661/pexels-photo-2474661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 
+  "https://images.pexels.com/photos/2087748/pexels-photo-2087748.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 
+  "https://images.pexels.com/photos/4194390/pexels-photo-4194390.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 
+  "https://images.pexels.com/photos/1437267/pexels-photo-1437267.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 
+  "https://images.pexels.com/photos/884600/pexels-photo-884600.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", 
+  "https://images.pexels.com/photos/2098085/pexels-photo-2098085.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" 
+]
 
-User.create!(
-  email: "user3@xyz.com",
-  password: "secret",
-    first_name: Faker::Name.female_first_name,
+
+
+
+current_role = ['Supervisor', 'Admin Manager', 'Contractor', 'Consultant', 'Advisor', 'Salesperson'] 
+current_ind =  ['Accounting', 'Finance', 'Health Care', 'Banking', 'Education']
+
+future_ind = ['Fullstack Engineering', 'Frontend Developer', 'Backend Developer', 'Data Analyst']
+
+i = 1
+30.times do
+  if i.odd?
+    @name1 = Faker::Name.female_first_name
+  else 
+    @name1 = Faker::Name.male_first_name
+  end
+  i += 1
+  User.create!(
+    first_name: @name1,
+    email: "#{@name1}@#{Faker::Internet.domain_name}",
+    password: "secret",
     last_name: Faker::Name.last_name,
     budget: [500, 1000, 1500, 2000, 2500, 3000].sample,
     timeframe: [6, 12, 18, 24].sample,
-    current_role: "Insurance Agent",
-    current_industry: "Insurance",
-    future_role: "Web Developer",
-    future_industry: "Software Engineering",
+    current_role: current_role.sample,
+    current_industry: current_ind.sample,
+    future_industry: future_ind.sample,
+    future_role: "",
     available_hrs_per_week: [12, 24, 36, 40, 60].sample,
-    status: '');
-    
-User.create!(
-  email: "user4@xyz.com",
-  password: "secret",
-    first_name: Faker::Name.male_first_name,
-    last_name: Faker::Name.last_name,
-    budget: [500, 1000, 1500, 2000, 2500, 3000].sample,
-    timeframe: [6, 12, 18, 24].sample,
-    current_role: "Stock Broker",
-    current_industry: "Equity",
-    future_role: "Web Developer",
-    future_industry: "Software Engineering",
-    available_hrs_per_week: [12, 24, 36, 40, 60].sample,
-    status: '');
+    status: '')
+end
+
+@users = User.all
+@users.each do |user|
+  user.future_role = user.future_industry
+  user.save
+end
+
+# User.create!(
+#   email: "user2@xyz.com",
+#   password: "secret",
+#     first_name: Faker::Name.male_first_name,
+#     last_name: Faker::Name.last_name,
+#     budget: [500, 1000, 1500, 2000, 2500, 3000].sample,
+#     timeframe: [6, 12, 18, 24].sample,
+#     current_role: "Wealth Planner",
+#     current_industry: "Banking",
+#     future_role: "Web Developer",
+#     future_industry: "Software Engineering",
+#     available_hrs_per_week: [12, 24, 36, 40, 60].sample,
+#     status: '');
+# 
+# User.create!(
+#   email: "user3@xyz.com",
+#   password: "secret",
+#     first_name: Faker::Name.female_first_name,
+#     last_name: Faker::Name.last_name,
+#     budget: [500, 1000, 1500, 2000, 2500, 3000].sample,
+#     timeframe: [6, 12, 18, 24].sample,
+#     current_role: "Insurance Agent",
+#     current_industry: "Insurance",
+#     future_role: "Web Developer",
+#     future_industry: "Software Engineering",
+#     available_hrs_per_week: [12, 24, 36, 40, 60].sample,
+#     status: '');
+#     
+# User.create!(
+#   email: "user4@xyz.com",
+#   password: "secret",
+#     first_name: Faker::Name.male_first_name,
+#     last_name: Faker::Name.last_name,
+#     budget: [500, 1000, 1500, 2000, 2500, 3000].sample,
+#     timeframe: [6, 12, 18, 24].sample,
+#     current_role: "Stock Broker",
+#     current_industry: "Equity",
+#     future_role: "Web Developer",
+#     future_industry: "Software Engineering",
+#     available_hrs_per_week: [12, 24, 36, 40, 60].sample,
+#     status: '');
+# 
