d172b0a665b231d7246e838279c9e3f8777f568f
Saikat Bhattacharya <saikatb@singnet.com.sg>
2021-06-12 14:36:47 +0800

    Show Specific Roadmap V1

--- app/controllers/roadmaps_controller.rb
+++ app/controllers/roadmaps_controller.rb
@@ -3,12 +3,13 @@ class RoadmapsController < ApplicationController
   end
 
   def show
+    @roadmap = Roadmap.find(params[:id])
+    @courses = @roadmap.courses
   end
 
   def suggested
     # get all roadmaps where roadmap user is not the current user
     # and roadmap user future role = current user future role
-
     sql_query = <<-SQL
     SELECT * FROM roadmaps INNER JOIN users ON users.id = roadmaps.user_id WHERE roadmaps.user_id <> '#{current_user.id}' AND users.future_role = '#{current_user.future_role}'
     SQL
