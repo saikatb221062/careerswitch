6e734d9152d2a1d3d39753abc9160396a2a8f1f0
Minyoung Chang <minyoungchang65@gmail.com>
2021-06-12 16:21:57 +0800

    stabilizie migration

--- db/migrate/20210610125642_add_course_id_roadmap_id_to_course_roadmaps.rb
+++ db/migrate/20210610125642_add_course_id_roadmap_id_to_course_roadmaps.rb
@@ -1,6 +1,6 @@
 class AddCourseIdRoadmapIdToCourseRoadmaps < ActiveRecord::Migration[6.0]
   def change
-    #add_reference :course_roadmaps, :course, index: true, #foreign_key: true
-    #add_reference :course_roadmaps, :roadmap, index: #true, foreign_key: true
+    add_reference :course_roadmaps, :course, index: true, foreign_key: true
+    add_reference :course_roadmaps, :roadmap, index: true, foreign_key: true
   end
 end
