8b8d5e9a3e5148e09ad6a515e1749549dc355bd3
Saikat Bhattacharya <saikatb@singnet.com.sg>
2021-06-16 07:09:32 +0800

    fix-link-back-from-see-roadmap-page

--- app/javascript/components/back_to_results.js
+++ app/javascript/components/back_to_results.js
@@ -2,21 +2,15 @@ const backToResults = () => {
   const backToResultsBtn = document.getElementById('back-to-results');
 
   if (backToResultsBtn) {
-    const queryString = location.search.substring(1);
-    const newString = queryString.replace(/%20/g, "+");
-    const tmpArray = newString.split("|")
-    const btnP2 = tmpArray[0];
-    const btnP4 = tmpArray[1];
-    // console.log(`${btnP2} ${btnP4}`)
-
     const backToResultsBtn = document.getElementById('back-to-results');
-    // console.log(backToResultsBtn.innerHTML);
+    console.log(backToResultsBtn.innerHTML);
+    const currentURL = sessionStorage.getItem('currentURL');
+    console.log(currentURL);
+    
+    const btnP1 = '<a href="'
+    const btnP3 = '" class="btn btn-outline-success rounded-pill"   style="float: left;">Back to Results</a>'
 
-    const btnP1 = '<a href="/results?query_from='
-    const btnP3 = '&query_to='
-    const btnP5 = '&commit=Search" class="btn btn-outline-success rounded-pill"   style="float: left;">Back to Results</a>'
-
-    backToResultsBtn.innerHTML = `${btnP1}${btnP2}${btnP3}${btnP4}${btnP5}`
+    backToResultsBtn.innerHTML = `${btnP1}${currentURL}${btnP3}`
   }
 }
 
