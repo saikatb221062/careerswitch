4ce362a8f336996c68209bcefc8e585256c4c848
Saikat Bhattacharya <saikatb@singnet.com.sg>
2021-06-19 07:17:56 +0800

    save-addnl-user-fields-at-signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
   before_action :persist_last_visited_path
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
   def after_sign_in_path_for(_resource)
@@ -8,6 +11,14 @@ class ApplicationController < ActionController::Base
     else
       root_path
     end
+  end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[current_role current_industry future_role])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[current_role current_industry future_role])
   end
 
   private
