0d1725ee453083635ddb14720baa89d08086e929
Saikat Bhattacharya <saikatb@singnet.com.sg>
2021-06-29 06:52:19 +0800

    see Other User Roadmap change and other changes

--- app/javascript/packs/back_to_results.js
+++ app/javascript/packs/back_to_results.js
@@ -1,14 +1,75 @@
 const backToResults = () => {
   const backToResultsBtn = document.getElementById('back-to-results');
 
-  if (backToResultsBtn) {
-    const backToResultsBtn = document.getElementById('back-to-results');
-    const currentURL = sessionStorage.getItem('currentURL');
-    
-    const btnP1 = '<a href="'
-    const btnP3 = '" class="btn btn-outline-success rounded-pill" style="float: left;">Back to Results</a>'
+  if (!backToResultsBtn) {
+    return;
+  };
 
-    backToResultsBtn.innerHTML = `${btnP1}${currentURL}${btnP3}`
+  const currentURL = sessionStorage.getItem('currentURL');
+  const btnP1 = '<a href="'
+  const btnP3 = '" class="btn btn-sm btn-secondary rounded-pill" style="float: left;">Back to Results</a>'
+
+  backToResultsBtn.innerHTML = `${btnP1}${currentURL}${btnP3}`
+
+  const cNames = Array(12).fill("");
+  const courseNames = document.querySelectorAll('.course');
+  
+  const coursesArray = makeCoursesArray(courseNames);
+
+  const startMth = parseInt(document.getElementById('rdmap-start').innerText);
+  const rdmapCNames = populateRdmapCourses(cNames, coursesArray, startMth);
+  
+  var rdmapCourse1 = document.getElementById('course-name-1');
+  rdmapCourse1.innerText = rdmapCNames[0];
+  var rdmapCourse2 = document.getElementById('course-name-2');
+  rdmapCourse2.innerText = rdmapCNames[1];
+  var rdmapCourse3 = document.getElementById('course-name-3');
+  rdmapCourse3.innerText = rdmapCNames[2];
+  var rdmapCourse4 = document.getElementById('course-name-4');
+  rdmapCourse4.innerText = rdmapCNames[3];
+
+  var rdmapCourse5 = document.getElementById('course-name-5');
+  rdmapCourse5.innerText = rdmapCNames[4];
+  var rdmapCourse6 = document.getElementById('course-name-6');
+  rdmapCourse6.innerText = rdmapCNames[5];
+  var rdmapCourse7 = document.getElementById('course-name-7');
+  rdmapCourse7.innerText = rdmapCNames[6];
+  var rdmapCourse8 = document.getElementById('course-name-8');
+  rdmapCourse8.innerText = rdmapCNames[7];
+
+  var rdmapCourse9 = document.getElementById('course-name-9');
+  rdmapCourse9.innerText = rdmapCNames[8];
+  var rdmapCourse10 = document.getElementById('course-name-10');
+  rdmapCourse10.innerText = rdmapCNames[9];
+  var rdmapCourse11 = document.getElementById('course-name-11');
+  rdmapCourse11.innerText = rdmapCNames[10];
+  var rdmapCourse12 = document.getElementById('course-name-12');
+  rdmapCourse12.innerText = rdmapCNames[11];
+
+  function makeCoursesArray(courseNames) 
+  {
+    const coursesArray = [];
+    courseNames.forEach(element => coursesArray.push(element.innerText));
+    return coursesArray;
+  };
+
+  function populateRdmapCourses(cNames, coursesArray, startMth) 
+  {
+    let j = 0;
+    for (var i = startMth - 1; i < 12; i++)
+    {
+      if ( j > (coursesArray.length - 1)) 
+      {
+        break
+      }
+      if (startMth == 0) 
+      {
+        break
+      }
+      cNames[i] = coursesArray[j];
+      j = j + 1; 
+    };
+    return cNames;
   }
 }
 
